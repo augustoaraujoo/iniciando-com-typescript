7b808f1b1237d8eb858e8ed7f42fb0438ae67b8c
augustoaraujoo <augustinho_darocha1311@hotmail.com>
2021-09-18 12:49:58 -0300

    typescript type annotation

--- modulos/01/app.js
+++ modulos/01/app.js
@@ -2,3 +2,7 @@
 //tsc --init
 var message = 'olá mundo </>';
 console.log(message);
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(1, 2));
--- modulos/01/app.ts
+++ modulos/01/app.ts
@@ -2,3 +2,8 @@
 //tsc --init
 const message : string = 'olá mundo </>';
 console.log(message);
+
+function addNumbers(num1:number,num2:number) {
+    return num1+num2;
+}
+console.log(addNumbers(1,2));
