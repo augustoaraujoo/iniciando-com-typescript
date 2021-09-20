f5e1937a0917794bc67b81a57cb7cb52ccee9d7a
augustoaraujoo <augustinho_darocha1311@hotmail.com>
2021-09-20 13:03:31 -0300

    created tuple.ts// exemples of tuple in ts

---
+++ modulos/mod06/tuple.js
@@ -0,0 +1,14 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var pessoa;
+pessoa = ['augusto', 33];
+console.log(pessoa[0]);
+var lista = ["a", "b", "c"];
+console.log(__spreadArray([], lista, true));
---
+++ modulos/mod06/tuple.ts
@@ -0,0 +1,8 @@
+let pessoa: [nome:string,idade:number];
+pessoa = ['augusto', 33]
+console.log(pessoa[0]);
+
+let lista: [string, ...string[]] = ["a", "b", "c"]
+console.log([...lista]);
+
+
